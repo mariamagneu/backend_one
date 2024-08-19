96122b1664eff46692d83fd5e01577f8d422663c
maria magdalena <neumariamagdalena@gmail.com>
2024-08-19 21:43:28 +0200

    create isAdmin check in middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,20 +1,28 @@
-const jwt = require('jsonwebtoken')
-const secret = require('../config/secretGenerator')
+const jwt = require("jsonwebtoken");
+const secret = require("../config/secretGenerator");
 
 const isAuthenticated = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1] // get the token from headers "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Njk4Y2Q3ZDFjNDc3YjMwNzE2OGM0YTMiLCJpYXQiOjE3MjEyOTMxNjB9.0u-Xwq483aEjrj9D2RUsDUZuqMTsOWFKeP_KZRW93Uw"
-    const payload = jwt.verify(token, secret) // decode token and get payload
+    const token = req.headers.authorization.split(" ")[1]; // get the token from headers "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Njk4Y2Q3ZDFjNDc3YjMwNzE2OGM0YTMiLCJpYXQiOjE3MjEyOTMxNjB9.0u-Xwq483aEjrj9D2RUsDUZuqMTsOWFKeP_KZRW93Uw"
+    const payload = jwt.verify(token, secret); // decode token and get payload
 
-    req.tokenPayload = payload // to pass the decoded payload to the next route
-    next()
+    req.tokenPayload = payload; // to pass the decoded payload to the next route
+    next();
   } catch (error) {
     // the middleware will catch error and send 401 if:
     // 1. There is no token
     // 2. Token is invalid
     // 3. There is no headers or authorization in req (no token)
-    res.status(401).json('token not provided or not valid')
+    res.status(401).json("token not provided or not valid");
   }
-}
+};
 
-module.exports = { isAuthenticated }
+const isAdmin = (req, res, next) => {
+  if (req.tokenPayload && req.tokenPayload.role === "Admin") {
+    next(); // User is an admin, allow access
+  } else {
+    res.status(403).json("Access denied. Admins only."); // Forbidden
+  }
+};
+
+module.exports = { isAuthenticated, isAdmin };
